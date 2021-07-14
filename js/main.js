60e22264cba1dbb0f4c4ccda459d05c9d749fa91
Geekscar <oscar.rodriguez.o@outlook.com>
2021-07-13 22:13:07 -0500

    Reasignación de esquema de colores y Comentado de CSS

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 let nav = document.getElementById('nav');
 let menu = document.getElementById('enlaces');
 let abrir = document.getElementById('open');
+let Nombre = document.getElementById('nombre');
 let botones = document.getElementsByClassName('btn-header');
 let cerrado = true;
 
@@ -15,12 +16,14 @@ function menus(){
         nav.style.transition = '1s';
         menu.style.top = '80px';
         abrir.style.color = "#fff"
+        Nombre.style.color = "#fff"
     }else{
         nav.classList.remove('nav1');
         nav.className = ('nav2');
         nav.style.transition = '1s';
         menu.style.top = '100px';
-        abrir.style.color = '#000';
+        //abrir.style.color = '#000';
+        //Nombre.style.color = "#000"
     }
 }
 
