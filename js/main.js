7b31f0653886c905411a289716f0218c68aab94e
Oscar Rodriguez <geekscar@users.noreply.github.com>
2021-07-14 21:33:14 -0500

    Ventana Modal, estilos e imágenes correspondientes

--- js/main.js
+++ js/main.js
@@ -6,6 +6,23 @@ let Nombre = document.getElementById('nombre');
 let botones = document.getElementsByClassName('btn-header');
 let cerrado = true;
 
+let cerrarModal1 = document.querySelectorAll(".close1")[0];
+let cerrarModal2 = document.querySelectorAll(".close2")[0];
+let cerrarModal3 = document.querySelectorAll(".close3")[0];
+let cerrarModal4 = document.querySelectorAll(".close4")[0];
+let abrirModal1 = document.querySelectorAll(".cc1")[0];
+let abrirModal2 = document.querySelectorAll(".cc2")[0];
+let abrirModal3 = document.querySelectorAll(".cc3")[0];
+let abrirModal4 = document.querySelectorAll(".cc4")[0];
+let modal1 = document.querySelectorAll(".modal1")[0];
+let modal2 = document.querySelectorAll(".modal2")[0];
+let modal3 = document.querySelectorAll(".modal3")[0];
+let modal4 = document.querySelectorAll(".modal4")[0];
+let modalC1 = document.querySelectorAll(".modal-container1")[0];
+let modalC2 = document.querySelectorAll(".modal-container2")[0];
+let modalC3 = document.querySelectorAll(".modal-container3")[0];
+let modalC4 = document.querySelectorAll(".modal-container4")[0];
+
 // Función de uso  barra de navegación transparente o degradada dependiendo de la posición de desplazamiento de la página
 function menus(){
     let Desplazamiento_Actual = window.pageYOffset;
@@ -21,9 +38,7 @@ function menus(){
         nav.classList.remove('nav1');
         nav.className = ('nav2');
         nav.style.transition = '1s';
-        menu.style.top = '100px';
-        //abrir.style.color = '#000';
-        //Nombre.style.color = "#000"
+        menu.style.top = '100px';        
     }
 }
 
@@ -77,3 +92,98 @@ window.addEventListener('click', function(e){
     }
 });
 
+// Ventana Modal 1//
+abrirModal1.addEventListener("click", function(e){
+    e.preventDefault();
+    modalC1.style.opacity = "1";
+    modalC1.style.visibility = "visible";
+    modal1.classList.toggle("modal-close1");
+});
+cerrarModal1.addEventListener("click", function(e){
+    modal1.classList.toggle("modal-close1");    
+    setTimeout(function(){
+        modalC1.style.opacity = "0";
+        modalC1.style.visibility = "hidden";
+    },850)
+});
+window.addEventListener("click", function(e){
+    if(e.target == modalC1){
+        modal1.classList.toggle("modal-close1");    
+        setTimeout(function(){
+            modalC1.style.opacity = "0";
+            modalC1.style.visibility = "hidden";
+    },850)
+    }    
+});
+
+// Ventana Modal 2//
+abrirModal2.addEventListener("click", function(e){
+    e.preventDefault();
+    modalC2.style.opacity = "1";
+    modalC2.style.visibility = "visible";
+    modal2.classList.toggle("modal-close2");
+});
+cerrarModal2.addEventListener("click", function(e){
+    modal2.classList.toggle("modal-close2");    
+    setTimeout(function(){
+        modalC2.style.opacity = "0";
+        modalC2.style.visibility = "hidden";
+    },850)
+});
+window.addEventListener("click", function(e){
+    if(e.target == modalC2){
+        modal2.classList.toggle("modal-close2");    
+        setTimeout(function(){
+            modalC2.style.opacity = "0";
+            modalC2.style.visibility = "hidden";
+    },850)
+    }    
+});
+
+// Ventana Modal 3//
+abrirModal3.addEventListener("click", function(e){
+    e.preventDefault();
+    modalC3.style.opacity = "1";
+    modalC3.style.visibility = "visible";
+    modal3.classList.toggle("modal-close3");
+});
+cerrarModal3.addEventListener("click", function(e){
+    modal3.classList.toggle("modal-close3");
+    setTimeout(function(){
+        modalC3.style.opacity = "0";
+        modalC3.style.visibility = "hidden";
+    },850)
+});
+window.addEventListener("click", function(e){
+    if(e.target == modalC3){
+        modal3.classList.toggle("modal-close3");    
+        setTimeout(function(){
+            modalC3.style.opacity = "0";
+            modalC3.style.visibility = "hidden";
+    },850)
+    }    
+});
+
+// Ventana Modal 4//
+abrirModal4.addEventListener("click", function(e){
+    e.preventDefault();
+    modalC4.style.opacity = "1";
+    modalC4.style.visibility = "visible";
+    modal4.classList.toggle("modal-close4");
+});
+cerrarModal4.addEventListener("click", function(e){
+    modal4.classList.toggle("modal-close4");
+    setTimeout(function(){
+        modalC4.style.opacity = "0";
+        modalC4.style.visibility = "hidden";
+    },850)
+});
+window.addEventListener("click", function(e){
+    if(e.target == modalC4){
+        modal4.classList.toggle("modal-close4");    
+        setTimeout(function(){
+            modalC4.style.opacity = "0";
+            modalC4.style.visibility = "hidden";
+    },850)
+    }    
+});
